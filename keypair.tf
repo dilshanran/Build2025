a88c8254ef1b8e31548a62a671529d82cbdaaadd
dilshanran <drmanawadu071@gmail.com>
2025-01-17 23:31:25 +0530

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "vprofilekey" {
   key_name   = "vprofilekey"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9uDRyPr/LzRPAbpUWDWjRC0AILi2VxDoW01wzfTnYm WILEY+dmanawadu@ZSRIL-9T196D3"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9uDRyPr"
 }
